9ef34c0660ee0670d194626d81b1ac263e97af14
Minji Kim <minji_0801@naver.com>
2021-12-16 19:31:56 +0900

    디데이 화면 UI 구성

---
+++ TimeCalculator/DdayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DdayViewController.swift
+//  TimeCalculator
+//
+//  Created by 김민지 on 2021/12/16.
+//
+
+import UIKit
+
+class DdayViewController: UIViewController {
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        calculateButton.layer.cornerRadius = 30
+    }
+    
+
+}
