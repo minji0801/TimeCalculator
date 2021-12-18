1301719baf832a6a2184f21fe40b3aa1d401a15e
Minji Kim <minji_0801@naver.com>
2021-12-18 16:57:59 +0900

    설정 화면 구현 및 카메라 화면 구성

--- TimeCalculator/SettingViewController.swift
+++ TimeCalculator/SettingViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import AVFoundation
+import MessageUI
 
 class SettingViewController: UIViewController {
     var player: AVAudioPlayer!
     
+    @IBOutlet weak var versionLabel: UILabel!
     @IBOutlet weak var darkModeButton: UIButton!
     @IBOutlet weak var soundButton: UIButton!
     @IBOutlet weak var reviewButton: UIButton!
@@ -18,13 +20,17 @@ class SettingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        versionLabel.text = "iOS v\(getCurrentVersion()) build \(getBuildNumber())"
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         AppearanceCheck(self)
         
-        [darkModeButton, soundButton, reviewButton, feedbackButton].forEach {
+        reviewButton.layer.borderColor = UIColor.systemGray.cgColor
+        reviewButton.layer.borderWidth = 1
+        
+        [darkModeButton, soundButton, feedbackButton].forEach {
             $0?.layer.borderWidth = 1
             if self.overrideUserInterfaceStyle == .light {
                 $0?.layer.borderColor = UIColor.black.cgColor
@@ -60,4 +66,92 @@ class SettingViewController: UIViewController {
         UserDefaults.standard.set(!soundOff, forKey: "SoundOff")
     }
     
+    // 앱 평가 버튼 클릭 시
+    @IBAction func reviewButtonTapped(_ sender: UIButton) {
+        // 스토어 url 열기
+//        if let url = URL(string: ""), UIApplication.shared.canOpenURL(url) {
+//            if #available(iOS 10.0, *) {
+//                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//            } else {
+//                UIApplication.shared.openURL(url)
+//            }
+//        }
+    }
+    
+    // 피드백 보내기 버튼 클릭 시
+    @IBAction func feedbackButtonTapped(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                             이곳에 피드백을 작성해 주시면 최대한 빨리 답변해 드리겠습니다.
+                             
+                             -------------------
+                             
+                             Device Model : \(self.getDeviceIdentifier())
+                             Device OS : \(UIDevice.current.systemVersion)
+                             App Version : \(self.getCurrentVersion())
+                             
+                             -------------------
+                             """
+            
+            composeViewController.setToRecipients(["timeCalculator.help@gmail.com"])
+            composeViewController.setSubject("<시간계산기> 피드백")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+            
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            print("메일 보내기 실패")
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                // 앱스토어로 이동하기(Mail)
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    
+    // Device Identifier 찾기
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+    
+    // 현재 버전 가져오기
+    func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+    
+    // 빌드 번호 가져오기
+    func getBuildNumber() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let build = dictionary["CFBundleVersion"] as? String else { return "" }
+        return build
+    }
+}
+
+extension SettingViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
